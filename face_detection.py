37b0b0ed3433ae6f649be15a804f3e67d6981ace
Sebastian Rys <sebastian.rys.95@gmail.com>
2020-01-05 18:41:04 +0100

    Added support for detection many faces and refactor code

--- face_detection.py
+++ face_detection.py
@@ -1,44 +1,83 @@
 import os
-import sys
-import pickle
 import cv2
 import numpy as np
-# import pandas as pd
 # import matplotlib.pyplot as plt
-# from skimage import io, color
-# from skimage.transform import resize
-from PIL import Image
 
-# from face_indetification import CASCADE_CLF
-CASCADE_CLF = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODELS_DIR = os.path.join(PROJECT_DIR, 'models')
+FILES_DIR = os.path.join(PROJECT_DIR, 'files')
 
 
 class FaceDetector:
     def __init__(self, detection_type):
         self._roi = None
         self._label = None
+        self._color_roi = None
         self._detection_type = detection_type.lower()
-        # print(self._detection_type)
 
-    def get_coordinates(self, _img_array):
+    def get_coordinates(self, _img_array, _multi_face):
         if self._detection_type == "base":
             _img_array = cv2.cvtColor(_img_array, cv2.COLOR_BGR2GRAY)
             return self.get_base_coordinates(_img_array)
         elif self._detection_type == "caffe":
-            return self.get_caffe_coordinates(_img_array)
+            return self.get_caffe_coordinates(_img_array, _multi_face)
 
-    def face_detection(self, _img_array, _img_path, _label):
+    def face_detection(self, _img_array, _img_path, _label, _multi_face=False):
         if self._detection_type == "base":
             self._roi, self._label = self.base_detection(_img_array, _label)
         elif self._detection_type == "caffe":
             _img_array_ = cv2.imread(_img_path)
-            self._roi, self._label = self.caffe_detection(_img_array_, _label)
-        return self._roi, self._label
+            self._roi, self._label, self._color_roi = self.caffe_detection(_img_array_, _label, _multi_face)
+        return self._roi, self._label, self._color_roi
+
+    def caffe_detection(self, _img_array, _label, _multi_face):
+        start_x, start_y, end_x, end_y = self.get_caffe_coordinates(_img_array, _multi_face)
+        mono_img_array = cv2.cvtColor(_img_array, cv2.COLOR_BGR2GRAY)
+        _roi = mono_img_array[start_y:end_y, start_x:end_x]
+        _color_roi = _img_array[start_y:end_y, start_x:end_x]
+        return _roi, _label, _color_roi
+
+    @staticmethod
+    def get_caffe_coordinates(_img_array, _multi_face=False, prototxt=os.path.join(FILES_DIR, "deploy.prototxt"),
+                              model=os.path.join(MODELS_DIR, "res10_300x300_ssd_iter_140000.caffemodel")):
+        def get_best_faces(multi_face):
+            confidences = []
+            for j in range(0, detections.shape[2]):
+                conf = detections[0, 0, j, 2]
+                if conf > 0.9:
+                    confidences.append(conf)
+            if confidences:
+                if multi_face:
+                    return confidences
+                return [max(confidences)]
+
+        # load model
+        net = cv2.dnn.readNetFromCaffe(prototxt, model)
+        # load image and construct an input blob for the image by resizing to a fixed 300x300 pixels and then normalizing it
+        (h, w) = _img_array.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(_img_array, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+        # pass the blob through the network and obtain the detections and predictions
+        net.setInput(blob)
+        detections = net.forward()
+
+        best_conf = get_best_faces(_multi_face)
+        faces = []
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence in best_conf:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (start_x_, start_y_, end_x_, end_y_) = box.astype("int")
+                if len(best_conf) == 1:
+                    return start_x_, start_y_, end_x_, end_y_
+                else:
+                    faces.append((start_x_, start_y_, end_x_, end_y_))
+        return faces
 
     @staticmethod
     def get_base_coordinates(_img_array):
         # find faces on image
-        faces = CASCADE_CLF.detectMultiScale(_img_array, scaleFactor=1.5, minNeighbors=5)
+        cascade_clf = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+        faces = cascade_clf.detectMultiScale(_img_array, scaleFactor=1.5, minNeighbors=5)
         if isinstance(faces, tuple):
             return
         else:
@@ -49,47 +88,5 @@ class FaceDetector:
 
     def base_detection(self, _img_array, _label):
         start_x, start_y, end_x, end_y = self.get_base_coordinates(_img_array)
-        _roi = self.get_roi(_img_array, start_x, start_y, end_x, end_y)
+        _roi = _img_array[start_y:end_y, start_x:end_x]
         return _roi, _label
-
-    @staticmethod
-    def get_caffe_coordinates(_img_array, prototxt="deploy.prototxt", model="res10_300x300_ssd_iter_140000.caffemodel"):
-        def get_best_face():
-            confidences = []
-            for j in range(0, detections.shape[2]):
-                conf = detections[0, 0, j, 2]
-                if conf > 0.9:
-                    confidences.append(conf)
-            if confidences:
-                return max(confidences)
-
-        # load model
-        net = cv2.dnn.readNetFromCaffe(prototxt, model)
-        # load image and construct an input blob for the image by resizing to a fixed 300x300 pixels and then normalizing it
-        # img = cv2.imread(_img_path)
-        (h, w) = _img_array.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(_img_array, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-        # pass the blob through the network and obtain the detections and predictions
-        net.setInput(blob)
-        detections = net.forward()
-
-        best_conf = get_best_face()
-        for i in range(0, detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-            if confidence == best_conf:
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (start_x_, start_y_, end_x_, end_y_) = box.astype("int")
-                return start_x_, start_y_, end_x_, end_y_
-
-    def caffe_detection(self, _img_array, _label):
-        start_x, start_y, end_x, end_y = self.get_caffe_coordinates(_img_array)
-        mono_img_array = cv2.cvtColor(_img_array, cv2.COLOR_BGR2GRAY)
-        _roi = self.get_roi(mono_img_array, start_x, start_y, end_x, end_y)
-        return _roi, _label
-
-    @staticmethod
-    def get_roi(_img_array, _start_x, _start_y, _end_x, _end_y):
-        current_roi = _img_array[_start_y:_end_y, _start_x:_end_x]
-        # current_roi = np.array(interest_region_gray, dtype="uint8")
-        # interest_region_gray.show()
-        return current_roi
